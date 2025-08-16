927e462aac0a9b1594333a08f7267a3baa635b94
Delphine FUMEX <delphinefumex@gmail.com>
2025-08-16 10:37:16 +0200

    Feat: Ajout de la classe Car

--- src/Model/Car.php
+++ src/Model/Car.php
@@ -2,37 +2,175 @@
 
 namespace App\Models;
 
-use PDO;
+use DateTimeImmutable;
 use InvalidArgumentException;
 
 
 /** 
- * Cette classe représente une voiture dans la base de données.
- * Elle contient également la logique pour la sauvegarde, la récupération et la modification
- * */
+ * Cette classe représente une voiture dans la BDD.
+ */
 
 class Car extends BaseModel
 {
 
     /**
-     * @var string Le nom de la table en base de données.
+     * @var string Le nom de la table en BDD.
      */
 
     protected string $table = 'cars';
 
 
-    /**
-     * @var array Propriétés privées représentant les colonnes de la table `cars`.
-     * L'encapsulation (via `private`) garantit que les données sont protégées et
-     * ne peuvent être modifiées que par les méthodes de cette classe (setters).
-     */
-    private ?int $id = null;
-    private string $brand;
-    private string $model;
-    private string $color;
-    private int $year;
-    private $power; // Comment faire pour enum???
-    private int $seatsNumber;
-    private string $registrationNumber;
-    private \DateTime $registrationDate;
+
+    function __construct(
+        private ?int $id = null,
+        private string $brand,
+        private string $model,
+        private string $color,
+        private int $year,
+        private string $power,
+        private int $seatsNumber,
+        private string $registrationNumber,
+        private \DateTimeImmutable $registrationDate
+    ) {
+
+        // Affectation avec valisation
+        $this->setBrand($brand)->setModel($model)->setColor($color)->setYear($year)->setPower($power)->setSeatsNumber($seatsNumber)->setRegistrationNumber($registrationNumber)->setRegistrationDate($registrationDate);
+    }
+
+    // ---------Les Getters ---------
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function getPower(): string
+    {
+        return $this->power;
+    }
+
+    public function getSeatsNumber(): int
+    {
+        return $this->seatsNumber;
+    }
+
+    public function getRegistrationNumber(): string
+    {
+        return $this->registrationNumber;
+    }
+
+    public function getRegistrationDate(): DateTimeImmutable
+    {
+        return $this->registrationDate;
+    }
+
+
+    // ---------Les Setters ---------
+
+    // Pas de setId car définit automatiquement par la BD
+
+    public function setBrand(string $brand): self
+    {
+        // Vérifier si la valeur n'est pas vide et utiliser trim pour que la valeur ne soit pas considéré comme rempli avec un espace
+        if (empty(trim($brand))) {
+            throw new InvalidArgumentException("La marque ne peut pas être vide.");
+        }
+        $this->brand = trim($brand);
+
+        return $this;
+    }
+
+    public function setModel(string $model): self
+    {
+        if (empty(trim($model))) {
+            throw new InvalidArgumentException("Le modéle ne peut pas être vide.");
+        }
+        $this->model = trim($model);
+
+        return $this;
+    }
+
+    public function setColor(string $color): self
+    {
+        if (empty(trim($color))) {
+            throw new InvalidArgumentException("La couleur ne peut pas être vide.");
+        }
+        $this->color = trim($color);
+
+        return $this;
+    }
+
+    public function setYear(int $year): self
+    {
+        $currentYear = (int)date('Y');
+        if ($year < 1900 || $year > $currentYear) {
+            throw new InvalidArgumentException("Année invalide.");
+        }
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function setPower(string $power): self
+    {
+        $allowed = ['thermique', 'electrique', 'hybride'];
+        if (!in_array(strtolower($power), $allowed, true)) {
+            throw new InvalidArgumentException("Type d'energie invalide.");
+        };
+
+        $this->power = trim($power);
+
+        return $this;
+    }
+
+    public function setSeatsNumber(int $seatsNumber): self
+    {
+        if ($seatsNumber <= 0 || $seatsNumber > 6) {
+            throw new InvalidArgumentException("Le nombre de siége ne peut pas être égale à 0 ou supérieure à 6");
+        }
+        $this->seatsNumber = $seatsNumber;
+
+        return $this;
+    }
+
+    public function setRegistrationNumber(string $registrationNumber): self
+    {
+        if (empty(trim($registrationNumber))) {
+            throw new InvalidArgumentException("La plaque d'immatriculation ne peut pas être vide.");
+        }
+        $this->registrationNumber = trim($registrationNumber);
+
+        return $this;
+    }
+
+    public function setRegistrationDate(DateTimeImmutable $registrationDate): self
+    {
+        $minDate = new DateTimeImmutable('1970-01-01');
+        if ($registrationDate < $minDate) {
+            throw new InvalidArgumentException("La date d'immatriculation est trop ancienne.");
+        }
+
+        $this->registrationDate = $registrationDate;
+
+        return $this;
+    }
 }
