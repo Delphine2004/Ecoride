146266f756f410a98bfbabf40ae4c9613eed7869
Delphine FUMEX <delphinefumex@gmail.com>
2025-09-09 16:10:39 +0200

    Feat : ajout de l'enum carColors

---
+++ src/Enum/CarColors.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enum;
+
+enum CarColors: string
+{
+    case NOIRE = "noire";
+    case BLANCHE = "blanche";
+    case BEIGE = "beige";
+    case ROUGE = "rouge";
+    case VERTE = "verte";
+    case BLEUE = "bleue";
+    case JAUNE = "jaune";
+    case ORANGE = "orange";
+    case VIOLETTE = "violette";
+}
