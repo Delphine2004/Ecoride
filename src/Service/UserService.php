ae8b7b0b2041fa0494b940daa6dc50b387126fea
Delphine FUMEX <delphinefumex@gmail.com>
2025-09-10 15:28:15 +0200

    Feat: ajout de createBooking + Fix: correction de la vérfication des utilisateurs

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Enum\UserRoles;
 use App\Repositories\UserRelationsRepository;
+use App\Repositories\CarRepository;
 use App\Models\User;
 use App\Services\BaseService;
 
@@ -14,7 +16,8 @@ class UserService extends BaseService
 
 
     public function __construct(
-        private UserRelationsRepository $userRelationsRepository
+        private UserRelationsRepository $userRelationsRepository,
+        private CarRepository $carRepository
     ) {
         parent::__construct();
     }
@@ -23,11 +26,91 @@ class UserService extends BaseService
     //----------Action VISITEUR------------
 
     // Permet à un visiteur de créer un compte
-    public function createAccount(array $data) // :int
+    public function createAccount(array $data): User
     {
-        //createUserFromForm() -- contient le hashage du mdp
+        // Vérifier que l'email n'est pas déjà utilisé
+        $existingUser = $this->userRelationsRepository->findUserByEmail($data['email']);
+
+        if ($existingUser) {
+            throw new InvalidArgumentException("Cet email est déjà utilisé.");
+        }
+
+
+        //Création de l'objet User vide
+        $user = new User(
+            $data['last_name'],
+            $data['first_name'],
+            $data['email'],
+            $data['password'],
+            false
+        );
+
+        $user->setUserLogin($data['login']);
+        $user->setUserPhone($data['phone']);
+        $user->setUserAddress($data['address']);
+        $user->setUserCity($data['city']);
+        $user->setUserZipCode($data['zip_code']);
+        $user->setUserUriPicture($data['picture']) ?? null;
+        $user->setUserCredits(($data['credits'] ?? 0) + 20);
+        // Le rôle passager est défini par défaut dans l'entité User
+
+        //Enregistrement dans la BD
+        $this->userRelationsRepository->insertUser($user);
+
+        return $user;
     }
 
+    // -----------------Actions PASSAGER ------------------
+
+    public function becomeDriver(array $data, int $passengerId): User
+    {
+        // Récupération du passager
+        $passenger = $this->userRelationsRepository->findUserById($passengerId);
+
+        // Vérification de l'existence du passeger
+        if (!$passenger) {
+            throw new InvalidArgumentException("Passager introuvable.");
+        }
+
+        // Vérification des permissions.
+        $this->ensurePassenger($passengerId);
+
+        $user = $this->userRelationsRepository->findUserById($passengerId);
+
+        // Ajout des champs relatifs au CONDUCTEUR
+        $user->setUserLicenceNo($data['licence_no']);
+        $user->addUserRole(UserRoles::CONDUCTEUR);
+
+
+        // Enregistrement dans dans BD.
+        $this->userRelationsRepository->updateUser($user);
+
+        return $user;
+    }
+
+    //----------Actions PASSAGER OU CONDUCTEUR ------------
+
+    // Permet à un utilisateur PASSAGER OU CONDUCTEUR de supprimer son compte.
+    public function deleteAccount(int $userId) //: bool
+    {
+        // Vérification des permissions
+        $this->ensurePassenger($userId);
+
+        if ($this->ensureAdmin($userId) || $this->ensureEmployee($userId)) {
+            throw new InvalidArgumentException("Un admin ou un employé ne peut pas supprimer son compte.");
+        }
+
+        // Récupération de l'utilisateur
+        $user = $this->userRelationsRepository->findUserById($userId);
+
+        // Vérification de l'existence de l'utilisateur
+        if (!$user) {
+            throw new InvalidArgumentException("Utilisateur introuvable.");
+        }
+
+        // Enregistrement en Bd
+        $this->userRelationsRepository->deleteUser($userId);
+    }
 
     //----------Actions TOUT ROLE------------
 
@@ -35,16 +118,27 @@ class UserService extends BaseService
     public function updateProfile(array $newData, int $userId): ?User
     {
 
-        // Vérification de la permission
-        $this->ensurePassenger($userId);
+        // Récupération de l'utilisateur
+        $user = $this->userRelationsRepository->findUserById($userId);
 
-        // Vérification de l'existance de l'utilisateur
-        if (!$userId) {
+        // Vérification de l'existence de l'utilisateur
+        if (!$user) {
             throw new InvalidArgumentException("Utilisateur introuvable.");
+        }
+
+        // Vérification des permissions.
+        if (
+            !$this->roleService->hasRole($userId, 'PASSAGER') &&
+            !$this->roleService->hasRole($userId, 'CONDUCTEUR') &&
+            !$this->roleService->hasRole($userId, 'EMPLOYE') &&
+            !$this->roleService->hasRole($userId, 'ADMIN')
+        ) {
+            throw new InvalidArgumentException("L'utilisateur n'est pas autorisé à modifier son profil.");
         }
 
         // Récupération de l'utilisateur
         $user = $this->userRelationsRepository->findUserById($userId);
+
 
         // Vérifications des champs modifiés et ajouts des nouvelles valeurs
         if (isset($newData['last_name'])) {
@@ -86,73 +180,77 @@ class UserService extends BaseService
     public function modifyPassword(string $newPassword, string $oldPassword, int $userId) //: bool
     {
 
-        // Vérification de la permission
-        $this->ensurePassenger($userId);
+        // Récupération de l'utilisateur
+        $user = $this->userRelationsRepository->findUserById($userId);
 
-        // Vérification de l'existance de l'utilisateur
-        if (!$userId) {
+        // Vérification de l'existence de l'utilisateur
+        if (!$user) {
             throw new InvalidArgumentException("Utilisateur introuvable.");
         }
 
-        /*
-        if (!$newPassword->verifyPassword($oldPassword)) {
-            throw new InvalidArgumentException("Le mot de passe actuel est incorrect.");
+        // Vérification des permissions.
+        if (
+            !$this->roleService->hasRole($userId, 'PASSAGER') &&
+            !$this->roleService->hasRole($userId, 'CONDUCTEUR') &&
+            !$this->roleService->hasRole($userId, 'EMPLOYE') &&
+            !$this->roleService->hasRole($userId, 'ADMIN')
+        ) {
+            throw new InvalidArgumentException("L'utilisateur n'est pas autorisé à modifier son profil.");
         }
-        $this->userRelationsRepository->validatePassword($newPassword);
-        $this->userRelationsRepository->setPassword($newPassword);*/
+
+
+        // Vérification du mot de passe
+        if ($newPassword = $oldPassword) {
+            throw new InvalidArgumentException("Le nouveau mot de passe doit être différent de l'ancien.");
+        }
+
+        // Récupération de l'utilisateur
+        $user = $this->userRelationsRepository->findUserById($userId);
+
+        $user->setUserPassword($newPassword);
     }
-
-
-    //----------Actions PASSAGER OU CONDUCTEUR ------------
-
-    // Permet à un utilisateur PASSAGER OU CONDUCTEUR de supprimer son compte.
-    public function deleteAccount(int $userId): void
-    {
-        // Vérification des permissions
-        $this->ensurePassenger($userId);
-
-        if ($this->ensureAdmin($userId) || $this->ensureEmployee($userId)) {
-            throw new InvalidArgumentException("Un admin ou un employé ne peut pas supprimer son compte.");
-        }
-
-        // Vérification de l'existance de l'utilisateur
-        if (!$userId) {
-            throw new InvalidArgumentException("Utilisateur introuvable.");
-        }
-
-        // Enregistrement en Bd
-        $this->userRelationsRepository->deleteUser($userId);
-    }
-
 
     //----------Actions ADMIN ------------
 
     // Permet à un admin de créer un compte employé.
-    public function createEmployeeAccount(array $data, int $adminId) //: int
+    public function createEmployeeAccount(array $data, int $adminId) // : ?User
     {
+
+
+        // Récupération de l'admin
+        $admin = $this->userRelationsRepository->findUserById($adminId);
+
+        // Vérification de l'existence de l'admin
+        if (!$admin) {
+            throw new InvalidArgumentException("Utilisateur introuvable.");
+        }
+
         //Verification de la permission
         $this->ensureAdmin($adminId);
-
-        // Vérification de l'existance de l'utilisateur
-        if (!$adminId) {
-            throw new InvalidArgumentException("Admin introuvable.");
-        }
     }
 
     // Permet à un admin de supprimer n'importe quel compte.
-    public function deleteAccountByAdmin(int $adminId, int $userId)
+    public function deleteAccountByAdmin(int $adminId, int $userId): void
     {
+
+
+
+        // Récupération de l'admin
+        $admin = $this->userRelationsRepository->findUserById($adminId);
+
+        // Vérification de l'existence de l'admin
+        if (!$admin) {
+            throw new InvalidArgumentException("Utilisateur introuvable.");
+        }
 
         //Verification de la permission
         $this->ensureAdmin($adminId);
 
-        // Vérification de l'existance de l'utilisateur
-        if (!$adminId) {
-            throw new InvalidArgumentException("Admin introuvable.");
-        }
+        // Récupération de l'utilisateur
+        $user = $this->userRelationsRepository->findUserById($userId);
 
-        // Vérification du statut de l'utilisateur
-        if (!$userId) {
+        // Vérification de l'existence de l'utilisateur
+        if (!$user) {
             throw new InvalidArgumentException("Utilisateur introuvable.");
         }
 
@@ -168,13 +266,17 @@ class UserService extends BaseService
     // Permet à un utilisateur PASSAGER de laisser un commentaire à un utilisateur CONDUCTEUR.
     public function leaveReview(int $passengerId): void
     {
+
+        // Récupération du passager
+        $passenger = $this->userRelationsRepository->findUserById($passengerId);
+
+        // Vérification de l'existence du passeger
+        if (!$passenger) {
+            throw new InvalidArgumentException("Passager introuvable.");
+        }
+
         // Vérification de la permission
         $this->ensurePassenger($passengerId);
-
-        // Vérification de l'existance de l'utilisateur
-        if (!$passengerId) {
-            throw new InvalidArgumentException("Utilisateur introuvable.");
-        }
     }
 
     //----------Actions EMPLOYEE ------------
@@ -182,12 +284,16 @@ class UserService extends BaseService
     //Fonction qui permet à un employé d'approuver un commentaire.
     public function approveReview(int $employeeId, string $review, int $rate): void
     {
-        // Vérification de la permission
-        $this->ensureEmployee($employeeId);
 
-        // Vérification de l'existance de l'utilisateur
-        if (!$employeeId) {
+        // Récupération de l'employé
+        $employee = $this->userRelationsRepository->findUserById($employeeId);
+
+        // Vérification de l'existence de l'employé
+        if (!$employee) {
             throw new InvalidArgumentException("Employé introuvable.");
         }
+
+        // Vérification de la permission
+        $this->ensureEmployee($employeeId);
     }
 }
