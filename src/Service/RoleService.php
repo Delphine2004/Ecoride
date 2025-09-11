78c9e62f20911dbef3f1753d181764a066b5a97f
Delphine FUMEX <delphinefumex@gmail.com>
2025-09-11 09:15:55 +0200

    Fix: utilisation de hasRole partout et création de hasAnyRol

--- src/Service/RoleService.php
+++ src/Service/RoleService.php
@@ -38,63 +38,44 @@ class RoleService
         return false;
     }
 
-    /**
-     * Vérifie si l'utilisateur a le rôle PASSAGER.
-     *
-     * @param integer $userId
-     * @return boolean
-     */
+
     public function isPassenger(int $userId): bool
     {
-        // Trouver les roles de l'utilisateur
-        $roles = $this->userRepository->getUserRoles($userId);
-
-        // Vérifier si le rôle qui lui est associé est correct
-        return in_array(UserRoles::PASSAGER, $roles, true);
+        return $this->hasRole($userId, 'PASSAGER');
     }
 
-    /**
-     * Vérifie si l'utilisateur a le rôle CONDUCTEUR.
-     *
-     * @param integer $userId
-     * @return boolean
-     */
+
     public function isDriver(int $userId): bool
     {
-        // Trouver les roles de l'utilisateur
-        $roles = $this->userRepository->getUserRoles($userId);
-
-        // Vérifier si le rôle qui lui est associé est correct
-        return in_array(UserRoles::CONDUCTEUR, $roles, true);
+        return $this->hasRole($userId, 'CONDUCTEUR');
     }
 
-    /**
-     * Vérifie si l'utilisateur a le rôle EMPLOYE.
-     *
-     * @param integer $userId
-     * @return boolean
-     */
+
+    public function isCustomer(int $userId): bool
+    {
+        return $this->hasAnyRole($userId, ['PASSAGER', 'CONDUCTEUR']);
+    }
+
+
     public function isEmployee(int $userId): bool
     {
-        // Trouver les roles de l'utilisateur
-        $roles = $this->userRepository->getUserRoles($userId);
-
-        // Vérifier si le rôle qui lui est associé est correct
-        return in_array(UserRoles::EMPLOYE, $roles, true);
+        return $this->hasRole($userId, 'EMPLOYE');
     }
 
-    /**
-     * Vérifie si l'utilisateur a le rôle ADMIN.
-     *
-     * @param integer $userId
-     * @return boolean
-     */
+
     public function isAdmin(int $userId): bool
     {
-        // Trouver les roles de l'utilisateur
-        $roles = $this->userRepository->getUserRoles($userId);
+        return $this->hasRole($userId, 'ADMIN');
+    }
 
-        // Vérifier si le rôle qui lui est associé est correct
-        return in_array(UserRoles::ADMIN, $roles, true);
+    public function isStaff(int $userId): bool
+    {
+        return $this->hasAnyRole($userId, ['EMPLOYE', 'ADMIN']);
+    }
+
+
+    public function isUser(int $userId): bool
+    {
+        return $this->hasAnyRole($userId, ['PASSAGER', 'CONDUCTEUR', 'EMPLOYE', 'ADMIN']);
     }
 }
