79b653eea885cd5e1b7a213e0ca7de36568cd429
Delphine FUMEX <delphinefumex@gmail.com>
2025-09-12 10:59:12 +0200

    feat: initialisation BaseController

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,3 +1,31 @@
 <?php
 
 namespace App\Controllers;
+
+abstract class BaseController
+{
+
+    protected function jsonResponse(array $data, int $statusCode = 200): void
+    {
+        http_response_code($statusCode);
+        header("Content-Type: application/json");
+        echo json_encode($data);
+        exit;
+    }
+
+    protected function successResponse(mixed $data, int $statusCode = 200): void
+    {
+        $this->jsonResponse(['success' => true, 'data' => $data], $statusCode);
+    }
+
+    protected function errorResponse(string $message, int $statusCode = 400): void
+    {
+        $this->jsonResponse(['success' => false, 'error' => $message], $statusCode);
+    }
+
+    protected function getJsonBody(): array
+    {
+        $input = file_get_contents("php://input");
+        return $input ? json_decode($input, true) ?? [] : [];
+    }
+}
