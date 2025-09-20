d9c1ca18043d5addb2678a09b68c3fba6a398e35
Delphine FUMEX <delphinefumex@gmail.com>
2025-09-20 15:26:02 +0200

    Fix: modification nom

--- src/Enum/BookingStatus.php
+++ src/Enum/BookingStatus.php
@@ -7,5 +7,5 @@ enum BookingStatus: string
     case CONFIRMEE = "Confirmée";
     case ENCOURS = "En cours";
     case ANNULEE = "Annulée";
-    case PASSEE = "Passée";
+    case FINALISE = "Finalisée";
 }
